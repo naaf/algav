8df20a7b13ffbd48d0d3bda09ec0f8fa028abbfb
naaf <serranashraf@gmail.com>
2015-11-26 19:53:19 +0100

    add operation briandais avec test

--- src/m1/algav/briandais/ArbreBRD.java
+++ src/m1/algav/briandais/ArbreBRD.java
@@ -7,11 +7,12 @@ public class ArbreBRD {
 	static final char FIN_MOT = '#';
 	static final char MOT_NULL = '\0';
 	static final char SEPERATOR_VER = '|';
-	static final char SEPERATOR_HOR = '-';
+	static final String SEPERATOR_HOR = "--";
+	static final String SPACE = "  ";
 
 	public static NoeudBRD constArbreBRD(String m) {
 		if ("".equals(m))
-			return new NoeudBRD();
+			return new NoeudBRD(FIN_MOT, null, null);
 		NoeudBRD root = new NoeudBRD(m.charAt(0), null, null);
 		NoeudBRD p = root;
 		for (int i = 1; i < m.length(); i++) {
@@ -38,7 +39,7 @@ public class ArbreBRD {
 
 	private static void printHorizontal(NoeudBRD abr, int largeur) {
 		if (abr == null) {
-			System.out.print(MOT_NULL);
+			// System.out.print(MOT_NULL);
 			return;
 		}
 		System.out.print(abr.getCle());
@@ -56,7 +57,7 @@ public class ArbreBRD {
 			System.out.print(SEPERATOR_VER);
 			nSpace = nbMaxFreres.get(i);
 			for (int j = 0; j < nSpace; j++) {
-				System.out.print(" ");
+				System.out.print(SPACE);
 			}
 		}
 		System.out.println("");
@@ -71,13 +72,19 @@ public class ArbreBRD {
 		List<Integer> nbMaxFreres = new ArrayList<>();
 		int i = 0;
 		int nb = 0;
+		int pnb = 1;
 		NoeudBRD p = racine;
 
 		do {
 
 			if (p == null) {
-				printVertical(nbMaxFreres, nb);
-				nb = 0;
+				pnb--;
+				if (pnb == 0) {
+					printVertical(nbMaxFreres, nb);
+					pnb = nb;
+					nb = 0;
+				}
+
 				p = fils(fileBRD.remove(0));
 				nbMaxFreres.remove(0);
 				i--;
@@ -86,7 +93,7 @@ public class ArbreBRD {
 
 				fileBRD.add(p);
 				nb++;
-				nbMaxFreres.add(nbMaxFrere(p));
+				nbMaxFreres.add(nbMaxFrere(fils(p)));
 
 				printHorizontal(fileBRD.get(i), nbMaxFreres.get(i));
 				p = p.getFrere();
@@ -96,9 +103,15 @@ public class ArbreBRD {
 		} while (!fileBRD.isEmpty() || p != null);
 	}
 
+	/**
+	 * 
+	 * @param abr
+	 * @param m
+	 * @return precondition m non vide
+	 */
 	public static NoeudBRD ajouterBRD(NoeudBRD abr, String m) {
 		if ("".equals(m)) {
-			return abr;
+			return new NoeudBRD(FIN_MOT, null, abr);
 		}
 		if (abr == null) {
 			return constArbreBRD(m);
@@ -117,4 +130,95 @@ public class ArbreBRD {
 		return abr;
 	}
 
+	public static int comptageMots(NoeudBRD abr) {
+		if (abr == null) {
+			return 0;
+		}
+		if (abr.getCle() == FIN_MOT) {
+			return 1 + comptageMots(abr.getFrere());
+		}
+		return  comptageMots(abr.getFils()) + comptageMots(abr.getFrere());
+	}
+
+	/**
+	 * 
+	 * @param abr
+	 * @param m
+	 * @return precondition m non vide
+	 */
+	public static int prefixe(NoeudBRD abr, String m) {
+		if (abr == null) {
+			return 0;
+		}
+		if ("".equals(m)) {
+			return comptageMots(abr);
+		}
+		if (abr.getCle() == m.charAt(0)) {
+			return prefixe(abr.getFils(), m.substring(1));
+		}
+		if (abr.getCle() < m.charAt(0)) {
+			return prefixe(abr.getFrere(), m);
+		} else {
+			return 0;
+		}
+	}
+
+	public static boolean recherche(NoeudBRD abr, String m) {
+		if (abr == null || "".equals(m)) {
+			return false;
+		}
+		int i = 0;
+		NoeudBRD p = abr;
+		while (p != null && i < m.length()) {
+			if (p.getCle() == m.charAt(i)) {
+				i++;
+				p = p.getFils();
+			} else if (p.getCle() < m.charAt(i)) {
+				p = p.getFrere();
+			} else {
+				return false;
+			}
+		}
+		if (p == null)
+			return false;
+		return true;
+	}
+
+	public static int hauteur(NoeudBRD abr) {
+		int a = 0, b = 0;
+		if (abr.getFils() == null && abr.getFrere() == null) {
+			return 0;
+		}
+		if (abr.getFils() != null)
+			a = hauteur(abr.getFils());
+		if (abr.getFrere() != null)
+			b = hauteur(abr.getFrere());
+		return Math.max(a, b) + 1;
+	}
+
+	public static int taille(NoeudBRD abr) {
+		if (abr == null) {
+			return 0;
+		}
+		return 1 + taille(abr.getFils()) + taille(abr.getFrere());
+	}
+
+	private static List<String> liste(NoeudBRD abr, StringBuffer sb, List<String> l) {
+		if (abr == null)
+			return null;
+		if (abr.getCle() == FIN_MOT)
+			l.add(sb.toString());
+
+		StringBuffer sb2 = new StringBuffer(sb);
+
+		sb.append(abr.getCle());
+		liste(abr.getFils(), sb, l);
+		liste(abr.getFrere(), sb2, l);
+
+		return l;
+	}
+
+	public static List<String> listeMots(NoeudBRD abr) {
+		return liste(abr, new StringBuffer(), new ArrayList<String>());
+	}
 }
--- src/m1/algav/briandais/test/TestArbreBRD.java
+++ src/m1/algav/briandais/test/TestArbreBRD.java
@@ -1,38 +1,100 @@
 package m1.algav.briandais.test;
 
+import static m1.algav.briandais.ArbreBRD.affiche;
+import static m1.algav.briandais.ArbreBRD.ajouterBRD;
+import static m1.algav.briandais.ArbreBRD.comptageMots;
+import static m1.algav.briandais.ArbreBRD.nbMaxFrere;
+import static m1.algav.briandais.ArbreBRD.prefixe;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import m1.algav.briandais.ArbreBRD;
 import m1.algav.briandais.NoeudBRD;
-import static m1.algav.briandais.ArbreBRD.*;
 
 public class TestArbreBRD {
 
+	static public NoeudBRD root;
+
+	@BeforeClass
+	public static void initData() {
+		root = ArbreBRD.constArbreBRD("Ashraf");
+		root = ajouterBRD(root, "Ash");
+		root = ajouterBRD(root, "zak");
+		root = ajouterBRD(root, "zag");
+		root = ajouterBRD(root, "zig");
+		root = ajouterBRD(root, "sinto");
+		root = ajouterBRD(root, "voila");
+		
+	}
+
+	@After
+	public void before() {
+		System.out.println("**************************");
+	}
+
 	@Test
 	public void testConstArbreBRD() {
-		NoeudBRD n  = ArbreBRD.constArbreBRD("Ashraf");
+		NoeudBRD n = ArbreBRD.constArbreBRD("Ashraf");
 		System.out.println(n);
 	}
-	
+
 	@Test
-	public void testNbMaxFrere(){
-		NoeudBRD n  = ArbreBRD.constArbreBRD("Ashraf");
-		System.out.println(nbMaxFrere(n));
+	public void testNbMaxFrere() {
+		
+		System.out.println("nbMaxFrere" + nbMaxFrere(root));
 	}
-	
+
 	@Test
-	public void testAffiche(){
-		NoeudBRD n  = ArbreBRD.constArbreBRD("Ashraf");
+	public void testAffiche() {
+		NoeudBRD n = ArbreBRD.constArbreBRD("Ashraf");
 		affiche(n);
 	}
+
+	@Test
+	public void testAjouterBRD() {
+		affiche(root);
+	}
+
+	@Test
+	public void testComptageMots() {
+		int c = comptageMots(root);
+		System.out.println("nb mots : " + c);
+	}
+
+	@Test
+	public void testPrefixe() {
+		int c = prefixe(root, "za");
+		System.out.println("nb prefixe za: " + c);
+	}
 	
 	@Test
-	public void testAjouterBRD(){
-		NoeudBRD n  = ArbreBRD.constArbreBRD("Ashraf");
-		n = ajouterBRD(n, "zak");
-		n = ajouterBRD(n, "zag");
-		n = ajouterBRD(n, "zig");
-		n = ajouterBRD(n, "sin2");
-		affiche(n);
+	public void testRecherche() {
+		System.out.println("recherche");
+		Assert.assertTrue("ash", ArbreBRD.recherche(root, "Ash"));
+		Assert.assertTrue("zak", ArbreBRD.recherche(root, "zak"));
+		Assert.assertFalse("zak", ArbreBRD.recherche(root, "sou"));
 	}
+	
+	@Test
+	public void testHauteur() {
+		int c = ArbreBRD.hauteur(root);
+		System.out.println("nb hauteur: " + c);
+	}
+	
+	@Test
+	public void testTaille() {
+		int c = ArbreBRD.taille(root);
+		System.out.println("nb taille: " + c);
+	}
+
+	@Test
+	public void testListeMots() {
+		System.out.println("listMots: " + ArbreBRD.listeMots(root));
+	}
+	
+	
+
 }
